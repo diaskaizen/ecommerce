018382cc2757df6292a4d435ca75636a0bc73440
tatakaizen <so1255998@gmail.com>
2022-01-30 23:08:04 +0100

    delette tebrary

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('admin/', admin.site.urls),
     path('', include('store.urls', namespace='store')),
+    path('', include('django_social_share.urls', namespace='social')),
 
 ]
 
